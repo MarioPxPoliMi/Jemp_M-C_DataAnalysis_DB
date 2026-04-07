ab46f7e02d6d9acbc1a1c7de15e839e5ae20a766
Mario Pennino <mario.pennino@mail.polimi.it>
2026-04-07 15:32:55 +0200

    import pandas

--- my_first_analysis.py
+++ my_first_analysis.py
@@ -1,5 +1,10 @@
+import pandas as pd
 
+storie = pd.read_csv('Storie.csv')
+post = pd.read_csv('Post.csv')
 
+storie["created_time"] = pd.to_datetime(storie["created_time"])
+post["created_time"] = pd.to_datetime(post["created_time"])
 
 
 
